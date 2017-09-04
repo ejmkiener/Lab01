a07acd2b336da63b1ff769233b88c4a3b08f0566
Eric <eric.j.m.kiener@gmail.com>
2017-09-03 22:50:33 -0300

    Latest changes

--- app/src/main/java/grupo001/dam/isi/frsf/utn/edu/ar/lab01/MainActivity.java
+++ app/src/main/java/grupo001/dam/isi/frsf/utn/edu/ar/lab01/MainActivity.java
@@ -1,30 +1,41 @@
 package grupo001.dam.isi.frsf.utn.edu.ar.lab01;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar skBar;
-    TextView skBarVal,importePlazoFijo;
-    EditText importeInicial;
+    TextView skBarVal,importePlazoFijo,operationMsg;
+    EditText correo,cuit,importeInicial;
     Button plazoFijoBtn;
+    DecimalFormat df;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        df=new DecimalFormat("#.##");
+        correo=(EditText)findViewById(R.id.correoET);
+        cuit=(EditText)findViewById(R.id.cuitET);
 
         importeInicial=(EditText)findViewById(R.id.importeInicial);
         skBar=(SeekBar)findViewById(R.id.seekBar1);
         skBarVal=(TextView) findViewById(R.id.seekBarVal);
         importePlazoFijo=(TextView) findViewById(R.id.importePlazoFijo);
         plazoFijoBtn=(Button) findViewById(R.id.plazoFijoBtn);
+
+        operationMsg=(TextView)findViewById(R.id.operationMsg);
 
         skBarVal.setText("");
         skBarVal.setText("..."+skBar.getProgress());
@@ -60,10 +71,11 @@ public class MainActivity extends AppCompatActivity {
 
                 if(!importeInicial.getText().toString().trim().equals("") /*importeInicial.getText().toString()!=""*/) {
                     importePlazoFijo.setText("");
-                    importePlazoFijo.setText("$" + GetInteres());
+                    importePlazoFijo.setText("$" + df.format(GetInteres()));
                 }else{
                     importePlazoFijo.setText("");
                 }
+
             }
 
             @Override
@@ -80,22 +92,39 @@ public class MainActivity extends AppCompatActivity {
         plazoFijoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //float interes=GetImportePlazoFijo()
-
+                if(!CheckFields()){
+                    operationMsg.setText("");
+                    operationMsg.setText(R.string.errorMsg);
+                    operationMsg.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.ERROR));
+                }else{
+                    operationMsg.setText("");
+                    operationMsg.setText("Plazo fijo realizado. Recibirá "+ df.format(GetImporteTotal())+ " al vencimiento.");
+                    operationMsg.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.CORRECTO));
+                }
             }
         });
     }
 
-    float GetImportePlazoFijo(float m,float i,int n){
-        float interes=m;
+    float GetImporteInteres(float m,float i,int n){
 
-        float aux= (float) Math.pow((1+(i/(float)100)),(n/(float)360));
+        float a=1+i;
+        float b=n/360f;
 
-        interes=interes*aux;
+        float aux= (float) Math.pow(a,b);
 
-        return interes;
+        return m*(aux-1);
     }
 
+    boolean CheckFields(){
+        String correoStr=correo.getText().toString();
+        String cuitStr=cuit.getText().toString();
+        String importeStr=importeInicial.getText().toString();
+        if(TextUtils.isEmpty(correoStr)||TextUtils.isEmpty(cuitStr)||TextUtils.isEmpty(importeStr)){
+            return false;
+        }
+
+        return true;
+    }
     float GetInteres(){
         String importe=importeInicial.getText().toString();
 
@@ -110,22 +139,40 @@ public class MainActivity extends AppCompatActivity {
         if(n>29){
             if(inicial>99999){
                 i=Float.parseFloat(getResources().getString(R.string.t10000030));
-            }else if(inicial>=5000){
-                i=Float.parseFloat(getResources().getString(R.string.t9999930));
             }else{
-                i=Float.parseFloat(getResources().getString(R.string.t500030));
+                if(inicial>5000){
+                    i=Float.parseFloat(getResources().getString(R.string.t9999930));
+                }else{
+                    i=Float.parseFloat(getResources().getString(R.string.t500030));
+                }
             }
         }else{
             if(inicial>99999){
                 i=Float.parseFloat(getResources().getString(R.string.t10000029));
-            }else if(inicial>=5000){
-                i=Float.parseFloat(getResources().getString(R.string.t9999929));
             }else{
-                i=Float.parseFloat(getResources().getString(R.string.t500029));
+                if(inicial>5000){
+                    i=Float.parseFloat(getResources().getString(R.string.t9999929));
+                }else{
+                    i=Float.parseFloat(getResources().getString(R.string.t500029));
+                }
             }
         }
 
 
-        return GetImportePlazoFijo(inicial,i,n);
+        return GetImporteInteres(inicial,i,n);
+    }
+
+    float GetImporteTotal(){
+        String importeStr=importeInicial.getText().toString();
+
+        float importe=0f;
+        int n=skBar.getProgress();
+        float i=0f;
+
+        if(importeStr!=null || importeStr!="") {
+            importe = Float.parseFloat(importeStr);
+        }
+
+        return importe+GetInteres();
     }
 }
